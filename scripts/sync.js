358be97b257ea11df5f671aca97e16e5725b48f9
chencheng <sorrycc@gmail.com>
2014-05-27 09:16:19 +0800

    config spm.source

--- scripts/sync.js
+++ scripts/sync.js
@@ -1,19 +1,26 @@
 'use strict';
+
+require('../config');
   
 var request = require('request');
 var spm = require('spm');
 var _ = require('lodash');
 
-request('http://spmjs.io/repository', function(err, res, body) {
+request(g_config.spm.source + 'repository', function(err, res, body) {
   var pkgs = JSON.parse(body);
+  installPkgs(pkgs);
+});
 
+function installPkgs(pkgs) {
   // class is not a valid spm package
   // https://github.com/pandorajs/class/pull/1
   pkgs = _.without(pkgs, 'class');
+
+  // install packages
   process.chdir('./spm-packages');
   spm.install({
-    source: 'http://spmjs.io/',
+    source: g_config.spm.source,
     query: pkgs,
     save: true
   });
-});
+}
